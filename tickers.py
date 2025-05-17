dec4e800dc7ea857f4b13fb56ab7e622f8cd1bd7
3xpl0it <3xpl0it.0@gmail.com>
2025-05-17 15:38:56 +0100

    first commit from working directory

---
+++ tickers.py
@@ -0,0 +1,27 @@
+# format: <BASE>-<QUOTE>-SWAP-LIN
+
+import requests
+
+def fetch_ticker_data():
+    url = f'https://api.ox.fun/v3/tickers'
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as error:
+        print('Error fetching ticker data:', error)
+
+all_data = fetch_ticker_data()
+
+tickernvolume = []
+
+for i in all_data["data"]:
+    tickernvolume.append([i["marketCode"], i["volume24h"], i["currencyVolume24h"]])
+
+top_10 = sorted(tickernvolume, key=lambda x: x[2], reverse=True)[:10]
+
+market_codes = []
+
+for i in top_10:
+    market_codes.append(i[0])
